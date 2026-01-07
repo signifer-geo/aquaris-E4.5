e5320fc6e6d6482b9145cd0876239c8c71068027
Claude User <claude-user@localhost>
2026-01-07 09:16:13 +0100

    add aquaris_build_id test localversion

--- kernel/init/main.c
+++ kernel/init/main.c
@@ -97,6 +97,9 @@ static inline void mark_rodata_ro(void) { }
 extern void tc_init(void);
 #endif
 
+const char aquaris_build_id[] = "AQUARIS_E4.5_TEST_KERNEL_2026_01";
+
+
 /*
  * Debug helper: via this flag we know that we are in 'early bootup code'
  * where only the boot processor is running with IRQ disabled.  This means
